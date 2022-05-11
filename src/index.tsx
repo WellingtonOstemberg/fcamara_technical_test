0ee92482eebfd7e996e28e12f009434dc0bac61b
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-11 16:33:47 -0400

    style: add Lato font as default font

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from 'styles/theme'
 import App from './App'
+import GlobalStyles from './styles/global'
 
 ReactDOM.render(
   <ThemeProvider theme={Theme}>
-    <App />
+    <>
+      <GlobalStyles />
+      <App />
+    </>
   </ThemeProvider>,
   document.getElementById('root'),
 )
