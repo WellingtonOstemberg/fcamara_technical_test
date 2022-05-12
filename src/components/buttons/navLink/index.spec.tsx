b4b708fa46bbf759b440adef1387a3bf8bdc2935
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-12 16:48:12 -0400

    test: BtnNavLink rendered successfully

---
+++ src/components/buttons/navLink/index.spec.tsx
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react'
+import { FiUsers } from 'react-icons/fi'
+import { colors } from '../../../styles/colors'
+import { TestWrapper } from '../../../utils'
+import { BtnNavLink } from '.'
+
+describe('BtnNavLink [Component]', () => {
+  it('Should render a button correctly', () => {
+    render(
+      <TestWrapper>
+        <BtnNavLink icon={<FiUsers />} text={'any text'} />
+      </TestWrapper>,
+    )
+    const btn = screen.getByRole('button', { name: 'any text' })
+    expect(btn).toBeInTheDocument()
+  })
+  it('Should render a button with right color and bg', () => {
+    render(
+      <TestWrapper>
+        <BtnNavLink icon={<FiUsers />} text={'any text'} />
+      </TestWrapper>,
+    )
+    const btn = screen.getByRole('button', { name: 'any text' })
+    expect(btn).toHaveStyle({
+      color: colors.gray700,
+      backgroundColor: '#fff',
+    })
+  })
+  it('Should render a button with right color and bg', () => {
+    render(
+      <TestWrapper>
+        <BtnNavLink icon={<FiUsers />} text={'any text'} active={true} />
+      </TestWrapper>,
+    )
+    const btn = screen.getByRole('button', { name: 'any text' })
+    expect(btn).toHaveStyle({
+      backgroundColor: colors.gray700,
+      color: '#fff',
+    })
+  })
+})
