8d5da46b812e236f66549a2747ad525a175e0718
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-12 07:43:28 -0400

    chore: config jest

---
+++ jest.config.ts
@@ -0,0 +1,27 @@
+export default {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/store/**/*.{ts,tsx}',
+    '!src/react-app-env.d.ts',
+    '!src/reportWebVitals.ts',
+    '!src/setupTests.ts',
+  ],
+  coveragePathIgnorePatterns: [
+    'src/pages/',
+    'src/components/index.ts',
+    'src/hooks/index.ts',
+    'src/utils/index.ts',
+    'src/ts/',
+    'src/redux/',
+    'src/routes/',
+    'src/services/',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+}
