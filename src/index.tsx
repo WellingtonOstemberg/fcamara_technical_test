30a5bff015822f1830ca420302f79907563d0347
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-12 19:28:53 -0400

    chore: configure react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from 'styles/theme'
 import App from './App'
 import GlobalStyles from './styles/global'
 
 ReactDOM.render(
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles />
-      <App />
-    </>
-  </ThemeProvider>,
+  <BrowserRouter>
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <App />
+      </>
+    </ThemeProvider>
+  </BrowserRouter>,
   document.getElementById('root'),
 )
