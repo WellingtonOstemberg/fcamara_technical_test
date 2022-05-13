dd8df0b71954ad6b6e81c13cce865fd40933655a
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-13 14:09:29 -0400

    chore: create Topbar

---
+++ src/components/cards/headers/campanha/components/topbar/index.tsx
@@ -0,0 +1,9 @@
+import { TopbarItem } from './components/navitem'
+import * as S from './styles'
+export const Topbar = () => {
+  return (
+    <S.Topbar>
+      <TopbarItem />
+    </S.Topbar>
+  )
+}
---
+++ src/components/cards/headers/campanha/components/topbar/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Topbar = styled.div`
+  height: 96px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
