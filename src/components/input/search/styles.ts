32840e4ca24c14033b3ecc4af195a4fcb892b6c5
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-13 14:25:53 -0400

    style: better visualization on SearchInputWrapper

--- src/components/input/search/styles.ts
+++ src/components/input/search/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 export const SearchInputWrapper = styled.div`
-  height: 48px;
-  width: 280px;
+  height: 50px;
+  width: 282px;
   display: flex;
-  border 1px solid ${(props) => props.theme.colors.bullet};
+  border: 1px solid ${(props) => props.theme.colors.bullet};
 `
 
 export const Input = styled.input`
@@ -18,7 +18,7 @@ export const Input = styled.input`
 export const SearchIconWrapper = styled.div`
   height: 32px;
   width: 32px;
-  margin-left: -40px;
+  margin-left: -45px;
   margin-top: 8px;
   display: flex;
   justify-content: center;
