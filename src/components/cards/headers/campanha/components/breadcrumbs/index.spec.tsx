d1c03c0a4575fef352be47ca7038388b5e6de7c6
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-13 10:23:14 -0400

    text: Breadcrumbs rendering correctly

---
+++ src/components/cards/headers/campanha/components/breadcrumbs/index.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import { TestWrapper, TestWrapperWithoutRoutes } from '../../../../../../utils'
+import { Breadcrumbs } from '.'
+import { MemoryRouter } from 'react-router-dom'
+
+describe('Breadcrumbs [Component]', () => {
+  it('Should render with correct text', () => {
+    render(
+      <TestWrapper>
+        <Breadcrumbs />
+      </TestWrapper>,
+    )
+    const breadcrumb = screen.getByText('Default')
+    expect(breadcrumb).toBeInTheDocument()
+  })
+  it('Should render with correct text Usuários', () => {
+    render(
+      <MemoryRouter initialEntries={[{ pathname: '/redes' }]}>
+        <TestWrapperWithoutRoutes>
+          <Breadcrumbs />
+        </TestWrapperWithoutRoutes>
+      </MemoryRouter>,
+    )
+    const breadcrumb = screen.getByText('Usuários')
+    expect(breadcrumb).toBeInTheDocument()
+  })
+})
