27e234bbc84906cc6772ff79e6b8848f1dacec10
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-13 05:22:12 -0400

    refactor: move ListItemAdmin to a separated file

--- src/components/menu/admin/index.tsx
+++ src/components/menu/admin/index.tsx
@@ -1,24 +1,14 @@
-import { useLocation } from 'react-router-dom'
 import { FiChevronDown } from 'react-icons/fi'
 import { menusAdminMock } from 'utils'
 import * as S from './styles'
+import { ListItemAdmin } from './components'
 
 export const MenuAdmin = () => {
-  const location = useLocation()
-
   return (
     <S.AdminWrapper>
       <S.ListAdmin>
         {menusAdminMock.map((menu) => {
-          return (
-            <S.ListItemAdmin
-              key={menu.path}
-              className={location.pathname === menu.path ? 'active' : ''}
-            >
-              <S.DropdownTextAdmin>{menu.name}</S.DropdownTextAdmin>{' '}
-              {menu.badge > 0 ? <S.BadgeAdmin>{menu.badge}</S.BadgeAdmin> : ''}
-            </S.ListItemAdmin>
-          )
+          return <ListItemAdmin key={menu.path} {...menu} />
         })}
       </S.ListAdmin>
       <S.DropdownAdmin>
