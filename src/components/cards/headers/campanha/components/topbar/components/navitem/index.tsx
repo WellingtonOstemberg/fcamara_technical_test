ad1fab564a1b999f2259e952f989ac944ce9a6a4
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-13 10:24:26 -0400

    chore: create TopbarItem

---
+++ src/components/cards/headers/campanha/components/topbar/components/navitem/index.tsx
@@ -0,0 +1,20 @@
+import { BtnPrimaryLifted } from 'components/buttons'
+import { SearchInput } from 'components/input'
+import { useLocation } from 'react-router-dom'
+import { NavItemWrapper } from './styles'
+
+export const TopbarItem = () => {
+  const location = useLocation()
+  switch (location.pathname) {
+    case '/redes':
+      return (
+        <NavItemWrapper>
+          <SearchInput />
+          <BtnPrimaryLifted text="Novo usuário" />
+        </NavItemWrapper>
+      )
+
+    default:
+      return <span />
+  }
+}
