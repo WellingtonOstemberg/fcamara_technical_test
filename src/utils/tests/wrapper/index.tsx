9c85f4ae8ab9f325f52267eea626c614dd60aa82
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-12 20:04:53 -0400

    refactor: update TestWrapperProps to use routing

--- src/utils/tests/wrapper/index.tsx
+++ src/utils/tests/wrapper/index.tsx
@@ -1,17 +1,22 @@
 import { ThemeProvider } from 'styled-components'
 import { Theme } from 'styles/theme'
 import GlobalStyles from 'styles/global'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 type TestWrapperProps = {
   children: JSX.Element
 }
 export const TestWrapper = ({ children }: TestWrapperProps) => {
   return (
-    <ThemeProvider theme={Theme}>
-      <>
-        <GlobalStyles />
-        {children}
-      </>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={Theme}>
+        <>
+          <GlobalStyles />
+          <Routes>
+            <Route path="*" element={<>{children}</>} />
+          </Routes>
+        </>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
