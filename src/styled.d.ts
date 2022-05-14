368d123a7c9e56aaf4035b1c58ceeb9479c18d1c
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-14 17:26:42 -0400

    refactor: typing styled component theme dinamyc

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,40 +1,12 @@
 // import original module declarations
 import 'styled-components'
+import { colors } from 'styles/colors'
+import { spacings } from 'styles/spacings'
 
 // and extend them!
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: {
-      orange: string
-      primary: string
-      secondary: string
-      primaryHover: string
-      gray: string
-      gray700: string
-      grayHover: string
-      grayLight: string
-      bullet: string
-      white: string
-      black: string
-      red: string
-      redHover: string
-      blue: string
-      blueDark: string
-      blueLight: string
-      disabled: string
-      overlay: string
-      background: string
-    }
-    spacings: {
-      xxSmall: string
-      xSmall: string
-      small: string
-      medium: string
-      large: string
-      xLarge: string
-      xxLarge: string
-      xxxLarge: string
-      xtreme: string
-    }
+    colors: typeof colors
+    spacings: typeof spacings
   }
 }
