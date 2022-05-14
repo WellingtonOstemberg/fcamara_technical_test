4849fb39f082d3cab2025d83eb9e7c03b5aefa66
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-14 17:24:36 -0400

    test: TabelUsersListItem rendering correctly

---
+++ src/components/table/users/components/tbody/components/userListItem/index.spec.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import { TestWrapper } from '../../../../../../../utils'
+import { usersMock } from '../../../../../../../utils/tests/mocks/users'
+import { TabelUsersListItem } from '.'
+
+describe('TabelUsersListItem [Component]', () => {
+  it('Should render table elements correctly', () => {
+    render(
+      <TestWrapper>
+        <table>
+          <tbody>
+            {usersMock.map(
+              (user, index) =>
+                index < 2 && (
+                  <TabelUsersListItem key={user.email} user={user} />
+                ),
+            )}
+          </tbody>
+        </table>
+      </TestWrapper>,
+    )
+    const tr = screen.getAllByRole('rowgroup')
+    for (let index = 0; index < tr.length; index++) {
+      expect(tr[index]).toBeInTheDocument()
+    }
+  })
+})
