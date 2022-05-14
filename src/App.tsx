a3b414675119dd62d2436b2c9072944e88359dfb
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-13 20:00:09 -0400

    refactor: move ThemeProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Layout } from 'layout'
+import { ThemeProvider } from 'styled-components'
+import { Theme } from 'styles/theme'
+import GlobalStyles from './styles/global'
 function App() {
   return (
-    <h1>App</h1>
-  );
+    <>
+      <ThemeProvider theme={Theme}>
+        <>
+          <GlobalStyles />
+          <Layout />
+        </>
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
-import { Theme } from 'styles/theme'
 import App from './App'
-import GlobalStyles from './styles/global'
 
 ReactDOM.render(
   <BrowserRouter>
-    <ThemeProvider theme={Theme}>
-      <>
-        <GlobalStyles />
-        <App />
-      </>
-    </ThemeProvider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root'),
 )
