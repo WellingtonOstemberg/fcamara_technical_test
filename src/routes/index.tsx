c3ae05634ec21b77e64efbc7be34bea6c590cb73
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-14 06:54:10 -0400

    refactor: element coming from mock

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,25 @@
 import { Home } from 'pages/home'
 import { Route, Routes } from 'react-router-dom'
-import { menusAdminMock } from 'utils'
+import { menusAdminMock, sidebarMock } from 'utils'
 
 export const MainRoutes = () => {
+  const routes = [
+    ...menusAdminMock,
+    ...sidebarMock,
+    {
+      path: 'usuarios/novo',
+      element: <Home />,
+    },
+    {
+      path: 'usuarios/editar/:id',
+      element: <Home />,
+    },
+  ]
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {menusAdminMock.map((menu) => {
-        return <Route key={menu.path} path={menu.path} element={<Home />} />
+      {routes.map((menu) => {
+        return <Route key={menu.path} path={menu.path} element={menu.element} />
       })}
     </Routes>
   )
