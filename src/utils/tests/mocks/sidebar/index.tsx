5be7ff20a92796ef36552c4f01e74a1e1b6852f8
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-14 17:26:05 -0400

    refactor: improuve mocks to test

--- src/utils/tests/mocks/sidebar/index.tsx
+++ src/utils/tests/mocks/sidebar/index.tsx
@@ -1,8 +1,14 @@
 import { FiUsers } from 'react-icons/fi'
 import { BsListCheck } from 'react-icons/bs'
 import { Home } from 'pages/home'
+import { Users } from 'pages/users'
 export const sidebarMock = [
-  { name: 'Usuários', path: '/usuarios', icon: <FiUsers />, element: <Home /> },
+  {
+    name: 'Usuários',
+    path: '/usuarios',
+    icon: <FiUsers />,
+    element: <Users />,
+  },
   {
     name: 'Perfis de Acesso',
     path: '/perfis-de-acesso',
