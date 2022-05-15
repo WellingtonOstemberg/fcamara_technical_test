85af7511c59f6c24d707b8596917e02db732ba6f
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-15 11:02:07 -0400

    chore: create SelectSimple

---
+++ src/components/selects/simple/index.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { generateMutableString } from 'utils'
+import { SelectSimpleContainer } from './styles'
+type SelectOptionsDataType = {
+  id: number
+  description: string
+}
+type SelectSimpleProps = {
+  label?: string
+  data?: SelectOptionsDataType[]
+}
+export const SelectSimple = ({ label, data }: SelectSimpleProps) => {
+  const [profilesListActive, setProfilesListActive] = useState(false)
+  const [profileSelected, setProfileSelected] = useState(
+    data && data.length > 0 ? data[0].id : 0,
+  )
+  const inputId = generateMutableString()
+
+  const handleProfileItem = (id: number) => {
+    setProfilesListActive((s) => !s)
+    setProfileSelected(id)
+  }
+
+  return (
+    <SelectSimpleContainer>
+      <label htmlFor={inputId}>{label}</label>
+      <div onClick={() => setProfilesListActive((s) => !s)}>
+        <span>
+          {profileSelected !== 0
+            ? data?.filter((profile) => profile.id === profileSelected)[0]
+                .description
+            : 'Selecione um perfil'}
+        </span>
+      </div>
+      <ul className={profilesListActive ? 'active' : ''} id={inputId}>
+        {data && data.length > 0 ? (
+          data.map((item) => {
+            return (
+              <li
+                className={profileSelected === item.id ? 'active' : ''}
+                key={item.id}
+                value={item.id}
+                onClick={() => handleProfileItem(item.id)}
+              >
+                {item.description}
+              </li>
+            )
+          })
+        ) : (
+          <option>Nenhum item a ser exibido</option>
+        )}
+      </ul>
+    </SelectSimpleContainer>
+  )
+}
---
+++ src/components/selects/simple/styles.ts
@@ -0,0 +1,64 @@
+import styled from 'styled-components'
+
+export const SelectSimpleContainer = styled.div`
+  height: 85px;
+  min-width: 540px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  margin-bottom: 8px;
+  & > label {
+    font-size: 14px;
+    display: block;
+    width: 100%;
+    height: 17px;
+    line-height: 17px;
+    font-weight: 400;
+    color: ${(props) => props.theme.colors.gray800};
+    margin-bottom: 8px;
+  }
+  & > div {
+    display: block;
+    min-height: 60px;
+    line-height: 60px;
+    font-size: 18px;
+    outline: none;
+    border: 1px solid ${(props) => props.theme.colors.black45};
+    border-radius: 4px;
+    padding-left: 10px;
+    cursor: pointer;
+  }
+  & > ul {
+    display: none;
+    z-index: 1000;
+  }
+  & > ul.active {
+    display: block;
+    position: relative;
+    margin-top: 5px;
+    min-width: 505px;
+    font-size: 18px;
+    outline: none;
+    padding: 16px;
+    border: 1px solid ${(props) => props.theme.colors.black45};
+    background-color: ${(props) => props.theme.colors.white};
+    border-radius: 4px;
+    & > li {
+      display: block;
+      margin: 5px 0px;
+      height: 32px;
+      line-height: 32px;
+      padding-left: 7px;
+      border-radius: 8px;
+      cursor: pointer;
+      &:hover {
+        background-color: ${(props) => props.theme.colors.orange};
+        color: ${(props) => props.theme.colors.white};
+      }
+    }
+    & > li.active {
+      background-color: ${(props) => props.theme.colors.orange};
+      color: ${(props) => props.theme.colors.white};
+    }
+  }
+`
