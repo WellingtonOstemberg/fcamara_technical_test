7eb1d2ca8f2ce64d1f4cf0e66ae522872f119f5d
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-18 14:51:07 -0400

    fix: create handlers at separated file

---
+++ src/components/cards/headers/campanha/components/topbar/components/navitem/utils/handlers/clicks/index.ts
@@ -0,0 +1,38 @@
+import Swal from 'sweetalert2'
+
+export const handleDelete = () => {
+  Swal.fire({
+    title: 'Excluir usuário',
+    text: 'Você deseja excluir este usuário definitivamente? Esta ação não poderá ser desfeita.',
+    icon: 'warning',
+    color: 'red',
+    showDenyButton: true,
+    confirmButtonText: 'Sim',
+    denyButtonText: `Não`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire('Usuário removido com sucesso!', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('A ação foi cancelada', '', 'error')
+    }
+  })
+}
+export const handleReset = () => {
+  Swal.fire({
+    title: 'Resetar senha',
+    text: 'Você deseja resetar a senha deste usuário?',
+    icon: 'warning',
+    color: 'red',
+    showDenyButton: true,
+    confirmButtonText: 'Sim',
+    denyButtonText: `Não`,
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire('Senha resetada com sucesso!', '', 'success')
+    } else if (result.isDenied) {
+      Swal.fire('A ação foi cancelada', '', 'error')
+    }
+  })
+}
