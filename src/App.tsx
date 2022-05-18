d517a259e3155d6446f67b17ff3ef72107c35625
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-18 16:20:28 -0400

    chore: add providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { Layout } from 'layout'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from 'styles/theme'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from 'store/configStore'
 import GlobalStyles from './styles/global'
 function App() {
   return (
     <>
       <ThemeProvider theme={Theme}>
-        <>
-          <GlobalStyles />
-          <Layout />
-        </>
+        <Provider store={store}>
+          <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
+            <>
+              <GlobalStyles />
+              <Layout />
+            </>
+          </PersistGate>
+        </Provider>
       </ThemeProvider>
     </>
   )
