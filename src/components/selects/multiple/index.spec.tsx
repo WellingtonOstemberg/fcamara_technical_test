439a21016ffa2137ac43cfe9b26a7be3b49eff47
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-18 12:32:14 -0400

    fix: correct useState mock implementation on test

--- src/components/selects/multiple/index.spec.tsx
+++ src/components/selects/multiple/index.spec.tsx
@@ -1,13 +1,22 @@
 import { render, screen } from '@testing-library/react'
 import { generateMutableString, TestWrapper } from '../../../utils'
 import { SelectMultiple } from '.'
+import React from 'react'
 
 describe('SelectMultiple [Component]', () => {
   it('Should render a input with correct label and no items message', () => {
     const labelTextMock = generateMutableString()
+    const setStateMocked = jest.fn()
+    const useStateMock: any = (useState: any) => [useState, setStateMocked]
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock)
+    const [selectedMock, setSelectedMock] = useStateMock([])
     render(
       <TestWrapper>
-        <SelectMultiple label={labelTextMock} />
+        <SelectMultiple
+          selected={selectedMock}
+          setSelected={setSelectedMock}
+          label={labelTextMock}
+        />
       </TestWrapper>,
     )
     const label = screen.getByLabelText(labelTextMock)
@@ -20,9 +29,15 @@ describe('SelectMultiple [Component]', () => {
   it('Should render a input with correct label and items', () => {
     const labelTextMock = generateMutableString()
     const descriptionMock = generateMutableString()
+    const setStateMocked = jest.fn()
+    const useStateMock: any = (useState: any) => [useState, setStateMocked]
+    jest.spyOn(React, 'useState').mockImplementation(useStateMock)
+    const [selectedMock, setSelectedMock] = useStateMock([])
     render(
       <TestWrapper>
         <SelectMultiple
+          selected={selectedMock}
+          setSelected={setSelectedMock}
           label={labelTextMock}
           data={[{ id: 1, description: descriptionMock }]}
         />
