da6aeaaedfb3a0528ee7ff1d3d0580a4d4214d7b
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-18 19:14:24 -0400

    refactor: bring users from store

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,29 +2,25 @@ import { CardHeaderCampanha, TableUsers } from 'components'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAllUsers } from 'services'
+import { useUsersAsyncSlice } from 'store/users'
 
 export const Users = () => {
   const navigate = useNavigate()
-  const users = useAllUsers()
+  const { getFetchUsers } = useAllUsers()
   const handleNavigate = () => {
     navigate('/usuarios/novo')
   }
 
+  const { usersAsyncSlice } = useUsersAsyncSlice()
+
   useEffect(() => {
-    users.getUsers()
+    getFetchUsers()
   }, [])
-  useEffect(() => {
-    console.log(users.allUsers), 'all'
-  }, [users.allUsers])
 
   return (
     <>
       <CardHeaderCampanha onClick={handleNavigate} />
-      {users.allUsers?.isLoading ? (
-        <h1>Loading...</h1>
-      ) : (
-        users.allUsers?.users && <TableUsers users={users.allUsers.users} />
-      )}
+      <TableUsers users={usersAsyncSlice.users} />
     </>
   )
 }
