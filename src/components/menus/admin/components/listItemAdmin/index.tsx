2a5ed9da59154aaef53326ac620a8de8428feac0
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-19 13:51:25 -0400

    refactor: remove route validator add active class on Configurações

--- src/components/menus/admin/components/listItemAdmin/index.tsx
+++ src/components/menus/admin/components/listItemAdmin/index.tsx
@@ -1,15 +1,13 @@
-import { useLocation } from 'react-router-dom'
 import * as S from '../../styles'
 export const ListItemAdmin = (menu: {
   name: string
   path: string
   badge: number
 }) => {
-  const location = useLocation()
   return (
     <S.ListItemAdmin
       key={menu.path}
-      className={location.pathname === menu.path ? 'active' : ''}
+      className={menu.name === 'Configurações' ? 'active' : ''}
     >
       <S.DropdownTextAdmin>{menu.name}</S.DropdownTextAdmin>{' '}
       {menu.badge > 0 ? <S.BadgeAdmin>{menu.badge}</S.BadgeAdmin> : ''}
