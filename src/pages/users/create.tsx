af0ca3ebf26cd5ccaa226841c6fb9d70c58bbdbf
Wellington Luiz Benedito Ostemberg <49499755+WellingtonOstemberg@users.noreply.github.com>
2022-05-19 11:54:08 -0400

    feat: add verification before save new user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -4,18 +4,29 @@ import { usePostUser } from 'services'
 import Swal from 'sweetalert2'
 import { UserType } from 'types'
 import { UserForm } from './components/form'
+import { validateUserFields } from './utils'
 
 export const UserCreate = () => {
   const [user, setUser] = useState<UserType>({})
   const { postFetchUser, usersAsyncSlice } = usePostUser()
-
   const handleSaveUser = () => {
-    Swal.fire({
-      text: 'Informações salvas com sucesso!',
-      icon: 'success',
-      width: '300px',
-    })
-    postFetchUser(user)
+    const { isValid } = validateUserFields(user)
+    if (isValid) {
+      Swal.fire({
+        text: 'Informações salvas com sucesso!',
+        icon: 'success',
+        width: '300px',
+      })
+      postFetchUser(user)
+    } else {
+      Swal.fire({
+        title: 'Erro ao cadastrar usuário',
+        text: 'Tenha certeza de que preencheu todos os campos.',
+        icon: 'error',
+        width: '300px',
+        heightAuto: true,
+      })
+    }
   }
 
   useEffect(() => {
